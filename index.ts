3d78c53e39f2e9e97a26f89bd99f659bac166986
Tim Anderson <tim.anderson@groundtruth.com>
2022-11-01 17:07:06 -0700

    add create application for app config

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
-import { AppConfigClient, CreateApplicationCommand, CreateApplicationCommandInput } from '@aws-sdk/client-appconfig';
+import { AppConfigClient, CreateApplicationCommand } from '@aws-sdk/client-appconfig';
 
 const awsRegion = 'us-east-1';
 
 console.log('starting');
 ( async () => {
     const client = new AppConfigClient({ region: awsRegion });
+    const command = new CreateApplicationCommand({Name: 'Ads_Manager_2', Description: 'app config POC', Tags: {
+        "owner_name": "tim",
+        "ttl": "2022",
+    }})
 })()
